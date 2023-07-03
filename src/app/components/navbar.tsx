af1a09a9ffe9bab3079d135026507376bbcdf8cc
Sipeng He <hsp8412@hotmail.com>
2023-07-02 19:12:36 -0600

    navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,68 @@
+'use client'
+import React, {useEffect, useState} from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEarthAsia, faBars, faXmark} from "@fortawesome/free-solid-svg-icons";
+
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            // If the window width is larger than 768px (md in Tailwind)
+            if (window.innerWidth > 768) {
+                setShowMenu(false); // Close the menu
+            }
+        };
+
+        // Attach the event listener
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const toggleShowMenu = ()=>{
+        setShowMenu(!showMenu)
+    }
+
+    const renderMenuIcon = () => {
+        if (!showMenu) return <FontAwesomeIcon icon={faBars} size="2x"/>;
+        return <FontAwesomeIcon icon={faXmark} size="2x" />;
+    };
+    const navItems = [
+        {label: "About Me", link:"/"},
+        {label:"Posts", link:"/posts"},
+        {label:"Projects", link:"/projects"},
+        {label:"Gallery", link:"/gallery"},
+        {label: "Contact", link:"/contact"}
+    ]
+
+    return (
+        <div>
+            <nav className="flex relative h-[80px] items-center justify-center md:justify-start px-10 border border-b-2">
+                <a className="md:me-10" href="/">
+                    <FontAwesomeIcon icon={faEarthAsia} size="2x" />
+                </a>
+                <ul className={`${showMenu?"left-0 h-[250px] z-1 bg-white":"left-[-100%] h-[250px]"} top-[80px] flex flex-col w-full absolute transition-all ease-in border-b-2 md:static md:flex-row md:h-full md:border-0`}>
+                    {navItems.map((item)=>(
+                        <li key={item.label} className="flex items-center">
+                            <a href={item.link} className="text-center w-full table hover:bg-gray-100 transition-all ease-in-out duration-200 rounded-0 font-bold text-2xl px-3 py-2 md:hover:rounded-3xl">
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="flex items-center absolute transform translate-x-full h-full left-0 top-0 md:hidden" onClick={toggleShowMenu}>
+                    <div className="">
+                        {renderMenuIcon()}
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      translate: {
+        '30': '30%',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
