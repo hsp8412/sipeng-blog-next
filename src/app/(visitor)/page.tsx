5acdae02fb62cea8f1450a137f1e39ae451afcbe
Sipeng He <hsp8412@hotmail.com>
2023-09-26 23:35:02 -0600

    Fix bugs

--- src/app/(visitor)/page.tsx
+++ src/app/(visitor)/page.tsx
@@ -21,7 +21,7 @@ const canadaFlag = "\uD83C\uDDE8\uD83C\uDDE6";
 const Page = () => {
   return (
     <div className="w-full">
-      <header className="relative mt-[-80px] z-[1]">
+      <header className="relative mt-[-80px] z-[1] w-full">
         <div className="absolute header-banner"></div>
         <div className="header-content absolute inset-1 flex flex-col justify-items-center items-center py-[80px] px-5 h-full">
           <h1 className="text-white text-2xl md:text-3xl mt-20 md:mt-36 font-bold">
